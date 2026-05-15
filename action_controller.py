b8b83eb7c33d7a84d9208950bead3b97cd0b4a8d
VishalAnand1001 <vishalsg1001@gmail.com>
2026-05-15 11:26:01 +0530

    Added cooldown system

--- action_controller.py
+++ action_controller.py
@@ -1,14 +1,26 @@
 import pyautogui
+import time
+
+last_action_time = 0
+cooldown = 1
 
 def control_action(gesture):
-    if gesture=="ONE":
-        pyautogui.press("volumeup")
-    elif gesture=="TWO":
-        pyautogui.press("volumedown")
-    elif gesture=="THREE":
-        pyautogui.scroll(-50)
-    elif gesture=="FOUR":
-        pyautogui.scroll(50)
-    elif gesture=="FIST":
-        pyautogui.press("esc")
+    global last_action_time
+    current_time = time.time()
+    if current_time - last_action_time < cooldown:
+        return
     
+    if gesture == "ONE":
+        pyautogui.press("volumeup")
+    elif gesture == "TWO":
+        pyautogui.press("volumedown")
+    elif gesture == "THREE":
+        pyautogui.scroll(-50)
+    elif gesture == "FOUR":
+        pyautogui.scroll(50)
+    elif gesture == "FIST":
+        pyautogui.press("esc")
+    else:
+        return
+    
+    last_action_time = current_time
